1947f0c82165e1803259dab5e046a4e16b9aa45a
saksham854 <sa9219@srmist.edu.in>
2026-03-03 08:41:46 +0530

    uc7

---
+++ step_program-feature-UC7-CharacterPattern-Class/uc_3.java
@@ -0,0 +1,26 @@
+public class uc_3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                " *****   *****   *****   ***** "));
+
+        System.out.println(String.join("",
+                "*     * *     * *     * *     *"));
+
+        System.out.println(String.join("",
+                "*     * *     * *     * *"));
+
+        System.out.println(String.join("",
+                "*     * *     * *****   ***** "));
+
+        System.out.println(String.join("",
+                "*     * *     * *           *"));
+
+        System.out.println(String.join("",
+                "*     * *     * *           *"));
+
+        System.out.println(String.join("",
+                " *****   *****   *       ***** "));
+    }
+}
